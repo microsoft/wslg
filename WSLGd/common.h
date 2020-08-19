8973d56a6dde1aeec44b6f37cb6cc6285f9b489d
Ben Hillis <benhill@ntdev.microsoft.com>
2020-08-19 10:19:21 -0700

    Move working directory to /mnt/wslg

--- WSLGd/common.h
+++ WSLGd/common.h
@@ -5,7 +5,7 @@
 #define LOG_ERROR(str, ...) { fprintf(stderr, "<3>WSLGd: %s:%u: " str "\n",__FUNCTION__, __LINE__, ##__VA_ARGS__); }
 #define LOG_INFO(str, ...) { fprintf(stderr, "<4>WSLGd: " str "\n", ##__VA_ARGS__); }
 #define RDP_PORT "3391"
-#define SHARE_PATH "/mnt/wsl"
+#define SHARE_PATH "/mnt/wslg"
 #define USERNAME "wslg"
 #define X11_RUNTIME_DIR SHARE_PATH "/.X11-unix"
 #define XDG_RUNTIME_DIR SHARE_PATH "/runtime-dir"
