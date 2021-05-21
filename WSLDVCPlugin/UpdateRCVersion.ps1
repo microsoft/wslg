e1ca78a1f2c7feb204029cc26f1526bb3e9ef8da
Vinicius Jarina <viniciusjarina@gmail.com>
2021-05-21 10:39:55 -0700

    Use Encoding `1252` for rc file

--- WSLDVCPlugin/UpdateRCVersion.ps1
+++ WSLDVCPlugin/UpdateRCVersion.ps1
@@ -3,10 +3,9 @@ $version = [string](gitversion /showvariable AssemblySemFileVer)
 $versionComma = $version.Replace(".", ",")
 $informationalVersion = [string](gitversion /showvariable InformationalVersion)
 
-# TODO: Add -Encoding "windows-1252" ? it seems is only supported on newer versions of PS
-$content = (Get-Content -Path ".\WSLDVCPlugin.rc")
+$content = (Get-Content -Encoding "windows-1252" -Path ".\WSLDVCPlugin.rc")
 $content = $content.Replace("1,0,0,1", $versionComma);
 $content = $content.Replace("1.0.0.1", $version);
 $content = $content.Replace("InformationalVersion", $InformationalVersion);
 
-Set-Content -Path ".\WSLDVCPlugin.rc" -Value $content
+Set-Content -Encoding "windows-1252" -Path ".\WSLDVCPlugin.rc" -Value $content
