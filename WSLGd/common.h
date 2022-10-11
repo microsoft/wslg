7a8f4381e70ba114e3111e817d3e8f7d3913c4c5
Hideyuki Nagase <hideyukn@ntdev.microsoft.com>
2022-10-11 11:56:27 -0700

    WSLGd: misc updates

--- WSLGd/common.h
+++ WSLGd/common.h
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 #pragma once
+#define SHARE_PATH "/mnt/wslg"
+#define USER_DISTRO_MOUNT_PATH SHARE_PATH "/distro"
 #define LOG_ERROR(str, ...) { fprintf(stderr, "<3>WSLGd: %s:%u: " str "\n",__FUNCTION__, __LINE__, ##__VA_ARGS__); }
 #define LOG_INFO(str, ...) { fprintf(stderr, "<4>WSLGd: " str "\n", ##__VA_ARGS__); }
